1f7a9037cea3b5d68a8bd9c6f726039a396060c7
Alireza <alirezazamanidev80@gmail.com>
2023-12-24 18:42:07 +0330

    Create edit chapter route

--- app/http/controllers/admin/course/chapter.controller.js
+++ app/http/controllers/admin/course/chapter.controller.js
@@ -5,7 +5,7 @@ const Controller = require("../../controller");
 const { StatusCodes: HttpStatus } = require("http-status-codes");
 const path = require("path");
 const mongoose=require('mongoose');
-const { deleteFileInPublic } = require("../../../../utils/functions");
+const { deleteFileInPublic, checkDataForUpdate } = require("../../../../utils/functions");
 const { CourseModel } = require("../../../../models/course");
 const chapter = require("../../../../routes/admin/chapter");
 const { CourseController } = require("./course.controller");
@@ -129,6 +129,31 @@ class ChapterController extends Controller {
 
     if(!chapters) throw createHttpError.NotFound("The course not founded!");
     return chapter;
+  }
+  async updateOneChapter(req,res,next){
+    try{
+      const {chapterId}=req.params;
+      await this.getOneChapter(chapterId);
+      const data=req.body;
+      checkDataForUpdate(data,['_id'])
+      const updateChapterResult=await CourseModel.updateOne({'chapters._id':chapterId},{
+        $set:{
+          'chapters.$':data
+        }
+      })
+      if(updateChapterResult.modifiedCount==0) throw createHttpError.InternalServerError();
+
+      return res.status(HttpStatus.OK).json({
+        data:{
+          statusCode:HttpStatus.OK,
+          message:"The chapter has been updated!"
+        }
+      })
+
+
+    }catch(err){
+      next(err);
+    }
   }
   async removeChapterById(req,res,next){
     try {
--- app/routes/admin/chapter.js
+++ app/routes/admin/chapter.js
@@ -4,6 +4,7 @@ const router=require('express').Router()
 router.put('/add',ChapterController.addChapter); // add chapter
 router.get('/list/:id',ChapterController.chaptersOfCourse);
 router.patch('/remove/:chapterId',ChapterController.removeChapterById);
+router.patch('/update/:chapterId',ChapterController.updateOneChapter);
 module.exports={
     AdminApiChapterRoutes:router
 }
