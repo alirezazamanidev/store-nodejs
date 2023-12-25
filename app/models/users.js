26a5cc8b0a545ec5982584e417bfbb6bb2203011
Alireza <alirezazamanidev80@gmail.com>
2023-12-26 02:10:02 +0330

    add basket schema in user model

--- app/models/users.js
+++ app/models/users.js
@@ -1,5 +1,17 @@
 const mongoose=require('mongoose');
 
+const productSchema=new mongoose.Schema({
+    productId:{type:mongoose.Types.ObjectId,ref:"product"},
+    count:{type:Number,default:1},
+});
+const courseSchema=new mongoose.Schema({
+    courseId:{type:mongoose.Types.ObjectId,ref:"Course"},
+    count:{type:Number,default:1},
+});
+const basketSchema=new mongoose.Schema({
+    course:{type:[courseSchema],default:[]},
+    product:{type:[productSchema],default:[]}
+})
 const Schema=new mongoose.Schema({
     first_name:{type:String},
     last_name:{type:String},
@@ -15,7 +27,8 @@ const Schema=new mongoose.Schema({
     diacount:{type:Number,default:0},
     birthDay:{type:String},
     Role:{type:String,default:'User'},
-    courses:{type:[mongoose.Types.ObjectId],ref:'Course',default:[]}
+    courses:{type:[mongoose.Types.ObjectId],ref:'Course',default:[]},
+    basket:{type:basketSchema}
 
 },{timestamps:true,toJSON:{virtuals:true}});
 
